bbe43f3acc363552c338a3ac2b3f886de13d70de
baswiel <b.vanderwiel@hotmail.com>
2015-05-25 12:33:12 +0200

    klein begin

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+    <meta charset="iso-8859-1">
+    <meta name="description" content="Forum">
+    <meta name="keywords" content="Forum bpsp">
+    <title>Forum bpsp</title>
+    <link rel="stylesheet" href="style/stylesheet.css">
+    <link rel="stylesheet" href="style/skeleton.css">
+    <link rel="stylesheet" href="style/normalize.css">
+
+</head>
+<body>
+<div class="Container">
+    <div class="Header">
+
+    </div>
+
+    <div class="row" id="Menubalk">
+        <div class=""
+        <div class="row">
+            <div class="twelve columns">
+                <ul id="nav">
+                    <li><a href="">Home</a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+
+            </div>
+
+
+        <div class="Body">
+
+    </div>
+
+</div>
+
+</body>
+
+<footer>
+    <div class="Footer">
+        <b>Copyright - 2015</b>
+    </div>
+</footer>
+</html>
